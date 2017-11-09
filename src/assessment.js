3120b9cc91cb9041157c0618ca798f4254332e7c
Roman Fuentes <romangof@gmail.com>
2017-11-09 17:52:26 -0300

    Building questions, still missing

--- src/assessment.js
+++ src/assessment.js
@@ -1,5 +1,48 @@
-class Assessment {
+import Dimensions from './dimensions';
 
+// function randomize(max) {
+//   return Math.floor( Math.random() * max )
+// }
+
+class Assessment {
+  constructor() {
+    this.questions = []
+    this.results = {}
+  }
+  
+  initializeResults() {
+    Dimensions.forEach( tag => {
+      this.results[tag.name] = 0
+    })
+  }
+  
+  buildQuestions() {
+    Dimensions.forEach( tag => {
+      i = 0
+      while (tag.answer.length != 0) {
+        this.questions.push({ 
+          'tag1': tag.name,  
+          'answer1': tag.answer,
+          'tag2': Dimensions[i+1].name,
+          'answer2': Dimensions[i++].answers[0]
+        })
+      }
+
+
+
+      // tag.answers.forEach( ans => {
+      //   console.log(ans)
+      //   ans
+      //   this.questions.push({ 
+      //     'tag1':tag.name,  
+      //     'answer1':tag.answer,
+      //     'tag2': Dimensions[i+1]
+      //   })
+      // })
+
+
+    })
+  }
 }
 
 export default Assessment;
